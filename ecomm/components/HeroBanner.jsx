1d2b4f4f3b24435282ed73da0eeaf5d2b61aa160
Desa <yagosan89@gmail.com>
2022-06-03 01:09:52 +0200

    Creation components + basic layout homepage
    Added global styles + basic infrastructure of HeroBanner.jsx

---
+++ ecomm/components/HeroBanner.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Link from 'next/link';
+
+const HeroBanner = () => {
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">RANDOM TEXT</p>
+        <h3>MID TEXT</h3>
+        <img src="" alt="headphones" className="hero-banner-image" />
+        <div>
+          <Link href="/product/ID">
+            <button type="button">BUTTON TEXT</button>
+          </Link>
+          <div cassName="desc">
+            <h5>Description</h5>
+            <p>DESCRIPTION</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default HeroBanner
