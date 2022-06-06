0002296c0c9c879a0a7c92ebf95c19104f31ff62
Desa <yagosan89@gmail.com>
2022-06-06 10:50:39 +0200

    Established elements of Layout

--- ecomm/components/Layout.jsx
+++ ecomm/components/Layout.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
 
-const Layout = () => {
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
   return (
-    <div>Layout</div>
+    <div className="Layout">
+      <Head>
+        <title>E-Commerce Demo</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">
+        {children}
+      </main>
+      <Footer />
+    </div>
   )
 }
 
